1da9638975fb5cae902360654596c54a8cc3bea6
zhengzongwei <zhengzongwei@foxmail.com>
2022-12-03 21:12:53 +0800

    note(go): 字符串操作

---
+++ Go语言从入门到进阶实战/src/chapter01/1.6.3-字符串操作.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	// theme := "你好 , World"
+	// comma := strings.Index(theme, ",")
+	// pos := strings.Index(theme[comma:],"ld")
+	// fmt.Printf(" %s\n", theme[comma:])
+	// fmt.Printf(" %s\n", theme[pos:])
+	// fmt.Printf(" %d", pos)
+
+	// angel := "Heros never die"
+
+	// angelBytes := []byte(angel)
+
+	// fmt.Printf("angelBytes %s \n", angelBytes)
+
+	// for i :=5; i<=10;i++{
+	// 	angelBytes[i] = ' '
+
+	// }
+	// fmt.Printf("angelBytes %s \n", string(angelBytes))
+
+	// hammer := "吃我一棒"
+
+	// sickle := "游戏结束"
+
+	// // 声明字节缓冲
+	// var stringBuilder bytes.Buffer
+	// stringBuilder.WriteString(hammer)
+	// stringBuilder.WriteString(sickle)
+
+	// fmt.Println(stringBuilder.String())
+
+}
